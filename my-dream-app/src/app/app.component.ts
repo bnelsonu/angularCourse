13b96a2cd971a4e2f04760d7c63ebbfc44d776e2
bnelsonu <bricardonelson@gmail.com>
2018-03-05 09:59:54 -0600

    first commit adding recipe files

---
+++ my-dream-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'] // you can add multiple style sheets
+})
+export class AppComponent {
+
+}
