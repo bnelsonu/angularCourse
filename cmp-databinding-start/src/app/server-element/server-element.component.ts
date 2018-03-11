ea08d4cb20ab717c4115f300c926a8ff242215bc
bnelsonu <bricardonelson@gmail.com>
2018-03-11 12:44:00 -0600

    adding servers and blueprints example

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  serverElements = [];
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onAddServer() {
+    this.serverElements.push({
+      type: 'server',
+      name: this.newServerName,
+      content: this.newServerContent
+    });
+  }
+
+}
